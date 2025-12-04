8968ec09ed47e22a858b0959e93ca43e51e8735d
Naman Kumar Bansal <Naman.24scse1180353@galgotiasuniversity.ac.in>
2025-12-04 20:25:19 +0530

    Refactor getters and setters in Job class
    Signed-off-by: Naman Kumar Bansal <Naman.24scse1180353@galgotiasuniversity.ac.in>

--- Job.java
+++ Job.java
@@ -11,19 +11,24 @@ public class Job {
     private int postedBy;
     private Timestamp postedAt;
 
-    public Job(){}
-    public int getId(){return id;}
-    public void setId(int id){this.id=id;}
-    public String getTitle(){return title;}
-    public void setTitle(String t){this.title=t;}
-    public String getCompany(){return company;}
-    public void setCompany(String c){this.company=c;}
-    public String getLocation(){return location;}
-    public void setLocation(String l){this.location=l;}
-    public String getDescription(){return description;}
-    public void setDescription(String d){this.description=d;}
-    public int getPostedBy(){return postedBy;}
-    public void setPostedBy(int p){this.postedBy=p;}
-    public Timestamp getPostedAt(){return postedAt;}
-    public void setPostedAt(Timestamp t){this.postedAt=t;}
+    public int getId() { return id; }
+    public void setId(int id) { this.id = id; }
+
+    public String getTitle() { return title; }
+    public void setTitle(String title) { this.title = title; }
+
+    public String getCompany() { return company; }
+    public void setCompany(String company) { this.company = company; }
+
+    public String getLocation() { return location; }
+    public void setLocation(String location) { this.location = location; }
+
+    public String getDescription() { return description; }
+    public void setDescription(String description) { this.description = description; }
+
+    public int getPostedBy() { return postedBy; }
+    public void setPostedBy(int postedBy) { this.postedBy = postedBy; }
+
+    public Timestamp getPostedAt() { return postedAt; }
+    public void setPostedAt(Timestamp postedAt) { this.postedAt = postedAt; }
 }
