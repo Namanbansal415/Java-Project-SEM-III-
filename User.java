9074de7433096a54f25e665ec3764de49ca5f518
Naman Kumar Bansal <Naman.24scse1180353@galgotiasuniversity.ac.in>
2025-12-04 20:20:41 +0530

    Refactor User class getters and setters for readability
    Signed-off-by: Naman Kumar Bansal <Naman.24scse1180353@galgotiasuniversity.ac.in>

--- User.java
+++ User.java
@@ -1,23 +1,59 @@
 package com.onlinejobportal.model;
 
 public class User {
+
     private int id;
     private String username;
     private String password;
     private String fullName;
     private String email;
     private String role;
-    public User() {}
-    public int getId(){return id;}
-    public void setId(int id){this.id=id;}
-    public String getUsername(){return username;}
-    public void setUsername(String u){this.username=u;}
-    public String getPassword(){return password;}
-    public void setPassword(String p){this.password=p;}
-    public String getFullName(){return fullName;}
-    public void setFullName(String f){this.fullName=f;}
-    public String getEmail(){return email;}
-    public void setEmail(String e){this.email=e;}
-    public String getRole(){return role;}
-    public void setRole(String r){this.role=r;}
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
 }
